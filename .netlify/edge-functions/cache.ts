33240dee067cb197a4b0a18f0196efba438ad243
Matt Kane <m@mk.gg>
2022-12-12 15:04:29 +0000

    chore: demo in-source conf

--- .netlify/edge-functions/cache.ts
+++ .netlify/edge-functions/cache.ts
@@ -2,3 +2,7 @@ export default async function handler(req: Request) {
   const hasCacheStorage = "CacheStorage" in globalThis;
   return Response.json({ hasCacheStorage });
 }
+
+export const config = () => ({
+  path: "/cache"
+})
