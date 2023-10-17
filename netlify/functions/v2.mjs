9e5e40bcbe4b0c524af4686acaabaf905c521de0
Matt Kane <m@mk.gg>
2023-10-17 20:45:35 +0100

    Simpify test case

--- netlify/functions/v2.mjs
+++ netlify/functions/v2.mjs
@@ -1,26 +1,7 @@
 export default function handler(request, context) {
-    setTimeout(() => {
-        console.log("timeout");
-    }, 9000);
-
-    const body = new ReadableStream({
-
-        start(controller) {
-            controller.enqueue("Wait...\n\n");
-            let i = 0
-            const timer = setInterval(() => {
-                console.log("interval", i);
-                controller.enqueue(`Hello, world! ${i}\n\n`);
-                if (i++ > 5) {
-                    controller.close();
-                    clearInterval(timer);
-                }
-            }, 1000);
-        },
-    });
-
-    return new Response(body);
+  setTimeout(() => console.log("timeout"), 5000);
+  return new Response("Hello from v2");
 }
 export const config = {
-    path: "/v2"
+  path: "/v2",
 };
