504e9f606a2455cb4b7313598e49757b74345881
Matt Kane <m@mk.gg>
2023-10-17 20:30:55 +0100

    Use stream for timer

--- netlify/functions/v2.mjs
+++ netlify/functions/v2.mjs
@@ -2,7 +2,23 @@ export default function handler(request, context) {
     setTimeout(() => {
         console.log("timeout");
     }, 5000);
-    return new Response("hello world");
+
+    const body = new ReadableStream({
+
+        start(controller) {
+            controller.enqueue("Wait...\n\n");
+            let i = 0
+            const timer = setInterval(() => {
+                controller.enqueue(`Hello, world! ${i}\n\n`);
+                if (i++ > 10) {
+                    controller.close();
+                    clearInterval(timer);
+                }
+            }, 1000);
+        },
+    });
+
+    return new Response(body);
 }
 export const config = {
     path: "/v2"
