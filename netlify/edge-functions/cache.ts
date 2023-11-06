104613ebaa61a14dcdae19f725b6841f78755060
Matt Kane <matt.kane@netlify.com>
2023-11-06 11:56:08 +0000

    Simplify

--- netlify/edge-functions/cache.ts
+++ netlify/edge-functions/cache.ts
@@ -1,7 +1,5 @@
 export default async function handler(req: Request) {
-  console.log(req)
-  const url = new URL(req.url)
-  return Response.json({ requrl: req.url, pathname: url.pathname, req: Object.entries(req), url });
+  return Response.json({ url: req.url });
 }
 
 export const config = {
