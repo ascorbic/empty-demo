919af66dfbce89c275c55c4233e23967611cee4a
Matt Kane <matt.kane@netlify.com>
2023-11-08 10:07:11 +0000

    cache manual

--- netlify/edge-functions/cache.ts
+++ netlify/edge-functions/cache.ts
@@ -1,7 +1,9 @@
 export default async function handler(req: Request) {
+  console.log("Request URL", req.url);
   return Response.json({ url: req.url });
 }
 
 export const config = {
   path: "/url/*",
+  cache: "manual"
 }
