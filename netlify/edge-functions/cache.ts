07ee2a62fef6e3d1840c0f228db22a01a87350f2
Matt Kane <matt.kane@netlify.com>
2024-01-09 14:26:40 +0000

    prefer-static-test

--- netlify/edge-functions/cache.ts
+++
@@ -1,9 +0,0 @@
-export default async function handler(req: Request) {
-  console.log("Request URL", req.url);
-  return Response.json({ url: req.url });
-}
-
-export const config = {
-  path: "/cached/*",
-  cache: "manual"
-}
--- netlify/functions/v2.ts
+++ netlify/functions/v2.ts
@@ -1,7 +1,9 @@
+import { Config } from "@netlify/functions";
 export default function handler(request, context) {
   setTimeout(() => console.log("timeout"), 5000);
   return new Response("Hello from v2");
 }
-export const config = {
-  path: "/v2",
+export const config: Config = {
+  path: "/*",
+  preferStatic: true,
 };
