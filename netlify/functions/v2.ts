b680f73fa4d8bedc65b27ca82fca1ec21448690f
Matt Kane <matt.kane@netlify.com>
2024-01-10 16:14:34 +0000

    Faster

--- netlify/functions/v2.ts
+++ netlify/functions/v2.ts
@@ -1,6 +1,5 @@
 import { Config } from "@netlify/functions";
 export default function handler(request, context) {
-  setTimeout(() => console.log("timeout"), 5000);
   return new Response("Hello from v2");
 }
 export const config: Config = {
